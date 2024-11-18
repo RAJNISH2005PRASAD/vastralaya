35529bec9f9aff5d7c6c74536ec817e71ab4cbb1
RAJNISH2005PRASAD <rajnishprasad2005@gmail.com>
2024-11-18 13:21:55 +0530

    fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,9 +8,9 @@ const Footer = () => {
         <div className="d-flex align-items-center justify-content-center pb-5">
           <div className="col-md-6">
             <p className="mb-3 mb-md-0">Made with ❤️ by {" "}
-              <a  href="" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Rajnish Prasad</a>
+              <Link  to="" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Rajnish Prasad</Link>
             </p>
-            <Link className="text-dark fs-4" href="https://github.com/RAJNISH2005PRASAD" target="_blank" rel="noreferrer">
+            <Link className="text-dark fs-4" to="https://github.com/RAJNISH2005PRASAD" target="_blank" rel="noreferrer">
               <i className="fa fa-github"></i>
             </Link>
           </div>
